02da44a0c5768cb2d353e8ac54b44ee4b23437c6
Deepthi295 <deepthireddy295@gmail.com>
2024-07-17 15:26:40 -0700

    wensday

---
+++ src/growmingvariables/LogicalOperaters.java
@@ -0,0 +1,15 @@
+package growmingvariables;
+
+public class LogicalOperaters {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Boolean b1 = true;
+Boolean b2 = false ;
+System.out.println("b1&&b2:"+(b1&&b2));
+System.out.println("b1||b2:"+(b1||b2));
+System.out.println("!(b1&&b2)"+!(b1&&b2));
+System.out.println("!(b1||b2)"+!(b1||b2));
+	}
+
+}
