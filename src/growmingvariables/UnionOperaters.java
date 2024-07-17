b41a5cf0dd2709ad9666733f745a80dd19330e3e
Deepthi295 <deepthireddy295@gmail.com>
2024-07-17 15:10:39 -0700

    Groowmingprograms

--- src/growmingvariables/UnionOperaters.java
+++ src/growmingvariables/UnionOperaters.java
@@ -4,7 +4,26 @@ public class UnionOperaters {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int num1 = 100;
+		int num2= 200;
+		
+		//- operator
+		int inversenum = -num1;
+		System.out.println("Oppsite of Number:"+inversenum);
 
+		//+ operator
+		num1++;
+		
+		//--decrement
+		num2--;
+		System.out.println("num1++ is:"+num1);
+		System.out.println("num2--is:"+num2);
+		
+		--num2;
+		++num1;
+		System.out.println("predecrement of num2"+num2);
+		System.out.println("preincrement of num1"+num1);
+		
 	}
 
 }
